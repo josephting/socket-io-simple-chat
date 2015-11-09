abf02ae4b4603d1852b88055957030781ae5f484
Joseph Ting <josephting90@gmail.com>
2015-11-09 18:59:29 +0800

    Initialize repository

---
+++ chat.js
@@ -0,0 +1,24 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(80);
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+    function(err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+io.on('connection', function(socket) {
+    socket.on('chat', function(data) {
+        socket.emit('chat', data);
+    });
+});
